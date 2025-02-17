600feb99bdcee5bd74a3bb67f1781ac255db7843
zishan <zishanali9766@gmail.com>
2025-02-17 14:54:50 +0530

    updated

--- backend/src/middlewares/errorHandler.middleware.ts
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -1,7 +1,21 @@
-import { ErrorRequestHandler } from "express";
-import { any } from "zod";
+import { ErrorRequestHandler,Response  } from "express";
+// import { any } from "zod";
 import { HTTPSTATUS } from "../config/http.config";
 import { AppError } from "../utils/appError";
+import { z, ZodError } from "zod";
+import { ErrorCodeEnum } from "../enums/error-code.enum";
+
+const formatZodError = (res: Response, error: z.ZodError) => {
+  const errors = error?.issues?.map((err) => ({
+    field: err.path.join("."),
+    message: err.message,
+  }));
+  return res.status(HTTPSTATUS.BAD_REQUEST).json({
+    message: "Validation failed",
+    errors: errors,
+    errorCode: ErrorCodeEnum.VALIDATION_ERROR,
+  });
+};
 
 export const errorHandler: ErrorRequestHandler = (
   error,
@@ -15,6 +29,10 @@ export const errorHandler: ErrorRequestHandler = (
     return res.status(HTTPSTATUS.BAD_REQUEST).json({
       message: "Invalid JSON format. Please check your request body.",
     });
+  }
+  
+  if (error instanceof ZodError) {
+    return formatZodError(res, error);
   }
   
   if (error instanceof AppError) {
